b64e5e2316958cdece2b24085d8a92cb0d602918
eyzenhart <eyzenhart@gmail.com>
2020-10-07 22:40:11 +0300

    Рефакторинг кода про добавление похожих волшебников

--- js/setup.js
+++ js/setup.js
@@ -4,11 +4,12 @@ var userDialog = document.querySelector('.setup');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var similarWizardList = document.querySelector('.setup-similar-list');
 
+var WIZARDS_NUMBER = 4;
+
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coat = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var coats = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyes = ['black', 'red', 'blue', 'yellow', 'green'];
-var wizards = [];
 
 
 userDialog.classList.remove('hidden');
@@ -19,40 +20,49 @@ var getRandomInt = function(max) {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-// Этот код не работает
 
-// var getWizards = function () {
-//   for (var i = 0; i <= 4; i++) {
-//     var wizardElement = similarWizardTemplate.cloneNode(true);
-//     wizardElement.querySelector('.setup-similar-label').textContent = names[getRandomInt(8)] + ' ' + surnames[getRandomInt(8)];
-//     wizardElement.querySelector('.wizard-coat').style.fill = coat[getRandomInt(6)];
-//     wizardElement.querySelector('.wizard-eyes').style.fill = eyes[getRandomInt(5)];
-//     wizards = wizards + wizardElement[i];
-//   }
-//   return wizards;
-// }
-
-// var fragment = document.createDocumentFragment();
-// for (var j = 0; j < wizards.length; j++) {
-//   fragment.appendChild(getWizards(wizards[i]));
-// }
-
-// similarWizardList.appendChild(fragment);
-
-
-// А этот работает
-
-var getWizards = function() {
-  for (var i = 0; i <= 4; i++) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
-    similarWizardList.appendChild(wizardElement);
-
-    wizardElement.querySelector('.setup-similar-label').textContent = names[getRandomInt(8)] + ' ' + surnames[getRandomInt(8)];
-    wizardElement.querySelector('.wizard-coat').style.fill = coat[getRandomInt(6)];
-    wizardElement.querySelector('.wizard-eyes').style.fill = eyes[getRandomInt(5)];
+var createWizardsData = function(number) {
+  var wizards = [];
+  for (var i = 0; i < number; i++) {
+    var wizard = {
+      coat: coats[getRandomInt(coats.length)],
+      eyes: eyes[getRandomInt(eyes.length)],
+      name: names[getRandomInt(names.length)] + ' ' + surnames[getRandomInt(surnames.length)]
+    };
+    wizards.push(wizard);
   }
+  return wizards;
 };
 
-getWizards();
+
+var createWizardElements = function(data) {
+  var wizardElements = [];
+  data.forEach(function(item) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = item.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = item.coat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = item.eyes;
+    wizardElements.push(wizardElement);
+  });
+  return wizardElements;
+};
+
+
+var renderWizards = function(elements) {
+  elements.forEach(function(item, index, array) {
+    similarWizardList.appendChild(item);
+  });
+};
+
+
+// создаем данные
+var wizardsData = createWizardsData(WIZARDS_NUMBER);
+
+//создаем элементы
+var wizardElements = createWizardElements(wizardsData);
+
+//отрисовываем элементы
+renderWizards(wizardElements);
+
 
 
